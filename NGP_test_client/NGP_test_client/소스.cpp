6eb3c7d53e75891a49b56875ea8ab69614fdb4b4
HongJinSun <hjs0913@naver.com>
2021-11-23 14:44:15 +0900

    update client

--- NGP_test_client/NGP_test_client/소스.cpp
+++ NGP_test_client/NGP_test_client/소스.cpp
@@ -4,12 +4,34 @@
 #include <WinSock2.h>
 #include <iostream>
 #include <string>
-#include "protocol.h"
+#include "C:\Users\Lenovo\Desktop\2021NGP-master\2021NGP-master\NGP\NGP\protocol.h"
 using namespace std;
 
 #define SERVERIP "127.0.0.1"
 #define SERVERPORT 9000
 #define BUFSIZE 512
+
+CLIENT_INFO clients[3];
+int start, game_time;
+int id;
+
+void C_UPDATE(SERVER_DATA server_data);
+void CS_MOVE(int key, SOCKET socket);
+
+DWORD WINAPI C_SAVE_PACKET(LPVOID arg)
+{
+	SOCKET sock = (SOCKET)arg;
+	SERVER_DATA server_data;
+
+	while (true) {
+		recv(sock, (char*)&server_data, sizeof(SERVER_DATA), 0);
+		
+		C_UPDATE(server_data);
+	}
+
+	closesocket(sock);
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
@@ -37,6 +59,9 @@ int main(int argc, char* argv[])
 	char* data[2]{ 0 };
 	int len;
 
+	HANDLE hThread1;
+	HANDLE hThread2;
+
 	while (true) {
 		CLIENT_DATA clientData;
 		clientData.id = 23;
@@ -49,8 +74,69 @@ int main(int argc, char* argv[])
 			break;
 		}
 
-		while (true);
+		hThread1 = CreateThread(NULL, 0, C_SAVE_PACKET, (LPVOID)sock, 0, NULL);
+		if (NULL == hThread1) {
+			closesocket(sock);
+		}
+		else {
+			CloseHandle(hThread1);
+		}
+
+		if (start != true)
+			continue;
+
+		while (true) {
+			int key;
+			key = getchar();
+			CS_MOVE(key, sock);
+		}
 	}
-	WSACleanup();
+	//WSACleanup();
 	return 0;
 }
+
+void C_UPDATE(SERVER_DATA server_data)
+{
+	switch (server_data.dataType)
+	{
+	case LOCATION: // 플레이어 위치값
+		clients[server_data.id].x = server_data.x;
+		clients[server_data.id].y = server_data.y;
+		clients[server_data.id].z = server_data.z;
+		break;
+	case LOGIN: // 플레이어 접속
+		id = server_data.id;
+		clients[server_data.id].alive = true;
+		clients[server_data.id].x = 0;
+		clients[server_data.id].y = 0;
+		clients[server_data.id].z = 0;
+		break;
+	case GAME_START:;   // 게임 시작
+		start = true;
+		break;
+	case TIME: // 시간
+		game_time = server_data.time;
+		break;
+	}
+}
+
+void CS_MOVE(int key, SOCKET socket)
+{
+	CLIENT_DATA client_data;
+	switch (key) {
+	case 'w':
+		client_data.id = id;
+		client_data.type = MOVE_FRONT;
+		send(socket, (char*)&client_data, sizeof(CLIENT_DATA), 0);
+		break;
+	case 'S':
+	case 's':
+		client_data.id = id;
+		client_data.type = MOVE_BACK;
+		send(socket, (char*)&client_data, sizeof(CLIENT_DATA), 0);
+		break;
+	default:
+		break;
+	}
+}
+
